58dc746e90a1b9db0de8d33a2f5a1a3e7df565f6
Matt Oxley <matt_ox@hotmail.com>
2024-02-27 09:17:59 +0100

    push notifications working

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState, useEffect} from 'react';
+import {MagicBellProvider} from '@magicbell/react-headless';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
-import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -17,17 +13,16 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
+
+const apiKey = 'd6a3cf19179a45a5daa9ac7f3f37e9d49914d2ad';
+const userEmail = 'matt@magicbell.io';
+const userKey = '5n4ooUtzydnYq5GYh6PIWGeP2alepTf/Qgb/Sp/g3Co=';
+const baseURL = 'http://localhost:3000';
 
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -55,44 +50,73 @@ function Section({children, title}: SectionProps): React.JSX.Element {
   );
 }
 
+function MBFetch(url: string, method: string, body: any) {
+  if (method === 'GET') {
+    return fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-MAGICBELL-API-KEY': apiKey,
+        'X-MAGICBELL-USER-EMAIL': userEmail,
+        'X-MAGICBELL-USER-HMAC': userKey,
+      },
+    });
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-MAGICBELL-API-KEY': apiKey,
+        'X-MAGICBELL-USER-EMAIL': userEmail,
+        'X-MAGICBELL-USER-HMAC': userKey,
+      },
+      body: JSON.stringify(body),
+    });
+  }
+}
+
 function App(): React.JSX.Element {
+  const [token, setToken] = useState('');
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  useEffect(() => {
+    PushNotificationIOS.requestPermissions().then((result: any) => {
+      console.log('request permissions: ', result);
+    });
+    PushNotificationIOS.addEventListener('register', (t: string) => {
+      console.log('token?', t);
+      setToken(t);
+    });
+  }, []);
+
+  useEffect(() => {
+    MBFetch(`${baseURL}/notifications`, 'POST', {});
+  }, []);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <MagicBellProvider apiKey={apiKey} userEmail={userEmail} userKey={userKey}>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <View
+            style={{
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            }}>
+            <Section title={`Device tokens?: ${token}`} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </MagicBellProvider>
   );
 }
 
@@ -112,6 +136,21 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  notificationContent: {
+    flex: 1,
+    borderColor: 'white',
+    borderWidth: 1,
+    margin: 5,
+    padding: 5,
+  },
+  notificationTitle: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: 'pink',
+  },
+  notificationBody: {
+    color: 'white',
   },
 });
 
