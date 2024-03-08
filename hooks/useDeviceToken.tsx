3c4a8c87459df9f1c2e566273ddfde932bc0ef9f
Matt Oxley <matt_ox@hotmail.com>
2024-03-08 20:27:19 +0100

    device token logic

--- hooks/useDeviceToken.tsx
+++ hooks/useDeviceToken.tsx
@@ -1,9 +1,7 @@
 import React, {useEffect} from 'react';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import {clientSettings} from '@magicbell/react-headless';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const key = 'MBsavedToken';
+import {decode as atob} from 'base-64';
 
 export default function useDeviceToken() {
   const [token, setToken] = React.useState<string | null>(null);
@@ -21,29 +19,34 @@ export default function useDeviceToken() {
     if (!token) {
       return;
     }
-    AsyncStorage.getItem(key).then(savedToken => {
-      if (savedToken === token) {
-        return;
-      }
-      const client = clientSettings.getState().getClient();
-      client
-        .request({
-          method: 'POST',
-          path: '/channels/mobile_push/apns/tokens',
-          data: {
-            apns: {
-              device_token: token,
+    const client = clientSettings.getState().getClient();
+    if (!client) {
+      return;
+    }
+    client
+      .request({
+        method: 'GET',
+        path: '/channels/mobile_push/apns/tokens',
+      })
+      .then((result: string[]) => {
+        const r = result.map((x: string) => JSON.parse(atob(x)).device_token);
+        if (r.includes(token)) {
+          return;
+        }
+        client
+          .request({
+            method: 'POST',
+            path: '/channels/mobile_push/apns/tokens',
+            data: {
+              apns: {
+                device_token: token,
+              },
             },
-          },
-        })
-        .then(result => {
-          console.log('post token', result);
-          AsyncStorage.setItem(key, token);
-        })
-        .catch(err => {
-          console.log('post token error', err);
-        });
-    });
+          })
+          .catch(err => {
+            console.log('post token error', err);
+          });
+      });
   }, [token]);
 
   return token;
