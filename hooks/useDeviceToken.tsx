c0bf75b2ce522cfda2315b677b9b0b16a0dc0940
Ullrich Schäfer <hi+github@ullrich.is>
2024-05-17 14:53:16 +0200

    Adjusting app to work with new APNS response
    Since the device token is now used as the ID there's no need to GET all tokens before sending the current one

--- hooks/useDeviceToken.tsx
+++ hooks/useDeviceToken.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import {clientSettings} from '@magicbell/react-headless';
-import {decode as atob} from 'base-64';
 
 export default function useDeviceToken() {
   const [token, setToken] = React.useState<string | null>(null);
@@ -21,32 +20,23 @@ export default function useDeviceToken() {
     }
     const client = clientSettings.getState().getClient();
     if (!client) {
+      console.error('Magicbell client expected');
       return;
     }
+    console.log('posting token', token);
     client
       .request({
-        method: 'GET',
+        method: 'POST',
         path: '/channels/mobile_push/apns/tokens',
+        data: {
+          apns: {
+            device_token: token,
+            installation_id: "development"
+          },
+        },
       })
-      .then((result: string[]) => {
-        const r = result.map((x: string) => JSON.parse(atob(x)).device_token);
-        if (r.includes(token)) {
-          return;
-        }
-        console.log('posting token', token);
-        client
-          .request({
-            method: 'POST',
-            path: '/channels/mobile_push/apns/tokens',
-            data: {
-              apns: {
-                device_token: token,
-              },
-            },
-          })
-          .catch(err => {
-            console.log('post token error', err);
-          });
+      .catch(err => {
+        console.log('post token error', err);
       });
   }, [token]);
 
