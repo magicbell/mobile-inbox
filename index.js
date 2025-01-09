360b0e76d5772074c1ed0e46732778e349c4c4a9
Ullrich Schäfer <hi+github@ullrich.is>
2025-01-09 13:34:58 +0100

    Removing old expo app.json

--- index.js
+++ index.js
@@ -4,11 +4,10 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
 import EventSource from 'react-native-sse';
 
 import 'react-native-url-polyfill/auto';
 
 global.EventSource = EventSource;
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('MobileInbox', () => App);
