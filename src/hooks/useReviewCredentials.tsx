ac22f575530a29f6a4d50d8ec546dd332e61aebc
Ullrich Schäfer <hi+github@ullrich.is>
2025-01-16 13:49:48 +0100

    chore: updating parsing of launch URL

--- src/hooks/useReviewCredentials.tsx
+++ src/hooks/useReviewCredentials.tsx
@@ -3,49 +3,34 @@ import { useLinkingURL } from 'expo-linking';
 import { Credentials } from './useAuth';
 import { useMemo } from 'react';
 
-function parseJwtToken(token: string) {
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split('')
-      .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join(''),
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
 /**
  * Parses a launch URL and if is a review environment connection URL, uses the embedded credentials to sign in to the app.
  *
  * ATTENTION: This is only for MagicBell internal use. You should not follow this example in your production app.
  *
  * Example URL:
- *  x-magicbell-review://connect?api-host=http://localhost:3000&project-jwt-token=abcdef.....abcdef
+ *   x-magicbell-review://connect?apiHost=[...]&apiKey=[...]&userEmail=[...]&userHmac=[...]
+ * 
  */
-const parseLaunchURLCredentials = (launchURL: URL): Credentials | null => {
-  const apiHost = launchURL.searchParams.get('api-host');
-  const projectJWTToken = launchURL.searchParams.get('project-jwt-token');
-  if (!apiHost || !projectJWTToken) {
-    console.warn(
-      "Expected parameters [ 'api-host', 'project-jwt-token' ] but got:",
-      Array.from(launchURL.searchParams.keys()),
-    );
+const parseLaunchURLCredentials = (url: URL): Credentials | null => {
+  const serverURL = url.searchParams.get('apiHost');
+  const apiKey = url.searchParams.get('apiKey');
+
+  // TODO: support userExternalID as well
+  const userEmail = url.searchParams.get('userEmail');
+
+  const userHmac = url.searchParams.get('userHmac');
+
+  if (!serverURL || !apiKey || !userEmail || !userHmac) {
+    console.warn('Could not parse credentials from launch URL: ', url);
     return null;
   }
-
-  const tokenData = parseJwtToken(projectJWTToken);
-  const apiKey = tokenData['ProjectKey']['APIKey'];
   const credentials: Credentials = {
-    apiKey: apiKey,
-    userEmail: 'review@magicbell.io',
-    userHmac: '',
-    serverURL: apiHost,
+    serverURL,
+    apiKey,
+    userHmac,
+    userEmail,
   };
-  console.log('Credentials from launch URL:', credentials);
   return credentials;
 };
 
