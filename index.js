4e7364059783fe0228648ba7fab56853700dd8ae
Ullrich Schäfer <hi+github@ullrich.is>
2025-01-16 12:29:48 +0100

    chore: marking polyfills as necessary

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+
+// Polyfills needed for @magicbell/react-headless
 import EventSource from 'react-native-sse';
-
 import 'react-native-url-polyfill/auto';
-
 global.EventSource = EventSource;
 
 AppRegistry.registerComponent('MobileInbox', () => App);
