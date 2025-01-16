8c9584502d188f79a7ce2b7d2881f78e9091b7c2
Ullrich Schäfer <hi+github@ullrich.is>
2025-01-16 12:27:18 +0100

    fix: App.tsx import

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 import EventSource from 'react-native-sse';
 
 import 'react-native-url-polyfill/auto';
