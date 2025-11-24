4a2b8b5ead1423b55b64d09dc579a498d3cefb38
Niya Gupta <niyabits@gmail.com>
2025-11-24 13:39:34 +0530

    feat: update appleTeamId and userJWT for APNs notifications

--- src/constants.ts
+++ src/constants.ts
@@ -77,7 +77,7 @@ export const config: { [key: string]: Credentials } = {
   prod: {
     serverURL: 'https://api.magicbell.com/v2',
     userJWT:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2VtYWlsIjpudWxsLCJ1c2VyX2V4dGVybmFsX2lkIjoiN2Y0YmFhYjUtMGM5MS00NGU4LThiNTgtNWZmODQ5NTM1MTc0IiwiYXBpX2tleSI6IjVmNWNmYjI5NTEzODQ2NDMzZTgxYjkxZWM1ZTkwOGM5NDNmZjYwNTgiLCJpYXQiOjE3NjM1NDMyOTksImV4cCI6MTc2MzYyOTY5OX0.NzZcuIv_g-nW0JAhF0i_pH4T96BHCfkdjkJOLnqvF6M',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2VtYWlsIjpudWxsLCJ1c2VyX2V4dGVybmFsX2lkIjoiN2Y0YmFhYjUtMGM5MS00NGU4LThiNTgtNWZmODQ5NTM1MTc0IiwiYXBpX2tleSI6IjVmNWNmYjI5NTEzODQ2NDMzZTgxYjkxZWM1ZTkwOGM5NDNmZjYwNTgiLCJpYXQiOjE3NjM5NzE1MTEsImV4cCI6MTc5NTUyOTExMX0.4Zty_YgwQKPsD8mNHqaneesrRe9XTtK5hlfj0RPl82Q',
   },
   local: {
     serverURL: 'https://1b35-79-153-3-135.ngrok-free.app',
